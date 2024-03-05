051eaa6a678bfc48a0570c79c375a73d9434cfa6
hieupth <64821726+hieupth@users.noreply.github.com>
2024-03-05 15:55:26 +0700

    chore: init python package

---
+++ python/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.md', encoding='utf-8') as file:
+    description = file.read()
+
+setup(
+    name='craftdet',
+    version='0.0.1',
+    packages=find_packages(),
+    license='Copyright (c) 2023 Hieu Pham',
+    zip_safe=True,
+    description='Implementation of CRAFT: Character Region Awareness For Text detection.',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='Hieu Pham',
+    author_email='64821726+hieupth@users.noreply.github.com',
+    url='https://gitlab.com/hieupth/pycraft',
+    keywords=[],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3'
+    ],
+)
