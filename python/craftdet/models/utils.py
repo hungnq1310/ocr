cc973e4315fc4948e35e611c9e62eee2d27df4dd
hieupth <64821726+hieupth@users.noreply.github.com>
2024-03-06 15:37:22 +0700

    chore: add load_model to model utils

--- python/craftdet/models/utils.py
+++ python/craftdet/models/utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from pathlib import Path
 from collections import OrderedDict
 
 
@@ -11,8 +12,8 @@ def init_weights(modules):
   for m in modules:
     if isinstance(m, nn.Conv2d):
       torch.nn.init.xavier_uniform_(m.weight.data)
-    if m.bias is not None:
-      m.bias.data.zero_()
+      if m.bias is not None:
+        m.bias.data.zero_()
     elif isinstance(m, nn.BatchNorm2d):
       m.weight.data.fill_(1)
       m.bias.data.zero_()
@@ -36,3 +37,26 @@ def copy_state_dict(state_dict):
         name = ".".join(k.split(".")[start_idx:])
         new_state_dict[name] = v
     return new_state_dict
+
+
+def load_model(model, weight_path: str, cuda: bool = True):
+  """
+  Load weight into model.
+  :param model:       target model.
+  :param weight_path: path to weight file.
+  :param cuda:        use cuda or not.
+  :return:            model with new weight.
+  """
+  weight_path = Path(weight_path).expanduser().resolve()
+  weight_path.parent.mkdir(exist_ok=True, parents=True)
+  weight_path = str(weight_path)
+  # load model into device.
+  if torch.cuda.is_available() and cuda:
+    model.load_state_dict(copy_state_dict(torch.load(weight_path)))
+    model = model.cuda()
+    model = torch.nn.DataParallel(model)
+    torch.backends.cudnn.benchmark = False
+  else:
+    model.load_state_dict(copy_state_dict(torch.load(weight_path, map_location='cpu')))
+  model.eval()
+  return model
